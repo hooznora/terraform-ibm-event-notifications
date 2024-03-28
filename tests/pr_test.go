0d88a6627a588fcf31bbd9d6f6c90fab679b5ded
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-03-28 10:58:12 +0000

    fix: examples

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -75,8 +75,6 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 func TestRunCompleteExample(t *testing.T) {
 	t.Parallel()
 
-	// t.Skip("Skipping upgrade test until initial code is in master branch")
-
 	options := setupOptions(t, "event-notification-complete", completeExampleDir)
 
 	output, err := options.RunTestConsistency()
@@ -86,8 +84,6 @@ func TestRunCompleteExample(t *testing.T) {
 
 func TestRunUpgradeExample(t *testing.T) {
 	t.Parallel()
-
-	t.Skip("Skipping upgrade test until initial code is in master branch")
 
 	options := setupOptions(t, "event-notification-upg", completeExampleDir)
 
@@ -101,8 +97,6 @@ func TestRunUpgradeExample(t *testing.T) {
 func TestRunFSCloudExample(t *testing.T) {
 	t.Parallel()
 
-	t.Skip("Skipping upgrade test until initial code is in master branch")
-
 	options := setupOptions(t, "en-fs", fsExampleDir)
 
 	output, err := options.RunTestConsistency()
@@ -112,8 +106,6 @@ func TestRunFSCloudExample(t *testing.T) {
 
 func TestDAInSchematics(t *testing.T) {
 	t.Parallel()
-
-	t.Skip("Skipping upgrade test until initial code is in master branch")
 
 	var region = validRegions[rand.Intn(len(validRegions))]
 
